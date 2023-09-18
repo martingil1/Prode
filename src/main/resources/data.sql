5b35a55795dc5f1704c14496f6415078fe1db84e
martingil1 <martinfacundogil@gmail.com>
2023-09-18 15:44:20 -0300

    Se cambiaron los valores de los id para la prueba de la carga de resultados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,18 @@
 
 INSERT INTO Result (id, name, local_team, visiting_team, gol_local_team, gol_visiting_team)
-VALUES (1,'Martin','Racing','Boca',1,1);
+VALUES (100,'Martin','Racing','Boca',1,1);
 INSERT INTO Result (id, name, local_team, visiting_team, gol_local_team, gol_visiting_team)
-VALUES (2,'Martin','River','GELP',2,1);
+VALUES (200,'Martin','River','GELP',2,1);
 INSERT INTO Result (id, name, local_team, visiting_team, gol_local_team, gol_visiting_team)
-VALUES (3,'Martin','Velez','Newells',0,0);
+VALUES (300,'Martin','Velez','Newells',0,0);
 INSERT INTO Result (id, name, local_team, visiting_team, gol_local_team, gol_visiting_team)
-VALUES (4,'Martin','Huracan','Central',1,1);
+VALUES (400,'Martin','Huracan','Central',1,1);
 
 INSERT INTO Result (id, name, local_team, visiting_team, gol_local_team, gol_visiting_team)
-VALUES (5,'Fecha1','Racing','Boca',2,1);
+VALUES (500,'Fecha1','Racing','Boca',2,1);
 INSERT INTO Result (id, name, local_team, visiting_team, gol_local_team, gol_visiting_team)
-VALUES (6,'Fecha1','River','GELP',2,1);
+VALUES (600,'Fecha1','River','GELP',2,1);
 INSERT INTO Result (id, name, local_team, visiting_team, gol_local_team, gol_visiting_team)
-VALUES (7,'Fecha1','Velez','Newells',0,0);
+VALUES (700,'Fecha1','Velez','Newells',0,0);
 INSERT INTO Result (id, name, local_team, visiting_team, gol_local_team, gol_visiting_team)
-VALUES (8,'Fecha1','Huracan','Central',1,1);
+VALUES (800,'Fecha1','Huracan','Central',1,1);
