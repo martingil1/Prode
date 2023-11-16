18137b98d321170f08326c367ec65cb39fa0e169
martingil1 <martinfacundogil@gmail.com>
2023-11-16 17:23:02 -0300

    Se agrego controlador para obtener los resultados parciales y totales

---
+++ src/main/java/com/example/prode/controllers/ScoreController.java
@@ -0,0 +1,42 @@
+package com.example.prode.controllers;
+
+import com.example.prode.dtos.ChargeResultsDto;
+import com.example.prode.responses.SumResultResponse;
+import com.example.prode.services.ScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/score")
+public class ScoreController {
+
+
+    @Autowired
+    ScoreService scoreService;
+
+    @GetMapping(value = "/suma-fecha/{nameUser}/{tourney}/{year}/{fecha}",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<SumResultResponse> getScoreByFecha(@PathVariable String nameUser,
+                                                           @PathVariable String tourney,
+                                                           @PathVariable Long year,
+                                                           @PathVariable Integer fecha) {
+
+        return ResponseEntity.ok(scoreService.showScorePartialByFechaAndUser(
+                new ChargeResultsDto(tourney, year, fecha, nameUser)));
+    }
+
+    @GetMapping(value = "/suma-torneo/{nameUser}/{tourney}/{year}",
+            produces = { MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<SumResultResponse> getScoreByTourney(@PathVariable String nameUser,
+                                                               @PathVariable String tourney,
+                                                               @PathVariable Long year){
+
+        return ResponseEntity.ok(scoreService.showScorePartialByTourneyAndUser(
+                new ChargeResultsDto(tourney, year, nameUser)));
+    }
+}
