20729666be743b323d75bd8a52459e189b0e6443
martingil1 <martinfacundogil@gmail.com>
2023-12-07 16:14:20 -0300

    Se modifico el mapeo al objeto FechaTourney y se agrego logica en el servicio de FechaTourney para cargar los resultados por fecha vacios y luego poder cargar los resultados de esa fecha parcialmente por dia o determinados partidos

--- src/main/java/com/example/prode/services/FechaTourneyServiceImpl.java
+++ src/main/java/com/example/prode/services/FechaTourneyServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.prode.services;
 
 import com.example.prode.dtos.ChargeResultsFechaDto;
 import com.example.prode.dtos.ResultDto;
+import com.example.prode.exceptions.FechaIsNotChargeException;
 import com.example.prode.exceptions.FechasIsAlreadyLoadedException;
 import com.example.prode.exceptions.TourneyNotExistException;
 import com.example.prode.models.FechaTourney;
@@ -16,7 +17,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+
+import static com.example.prode.utils.Constants.FECHA;
 
 @Service
 public class FechaTourneyServiceImpl implements FechaTourneyService{
@@ -26,35 +30,93 @@ public class FechaTourneyServiceImpl implements FechaTourneyService{
     @Autowired
     TourneyRepository tourneyRepository;
 
-    public static final String FECHA = "Fecha ";
-
     @Override
-    public ChargeResultResponse chargeResultFecha(ChargeResultsFechaDto chargeResultsFechaDto) {
+    public ChargeResultResponse chargeFechaEmpty(ChargeResultsFechaDto chargeResultsFechaDto) {
 
         List<Result> results = new ArrayList<>();
-        ChargeResultResponse response = new ChargeResultResponse();
-
         Tourney tourney =
                 tourneyRepository.getTourneyByNameAndYearTourney(
-                        chargeResultsFechaDto.getNameTourney(), chargeResultsFechaDto.getYear())
+                                chargeResultsFechaDto.getNameTourney(), chargeResultsFechaDto.getYear())
                         .orElseThrow(TourneyNotExistException::new);
 
-        if(fechaTourneyRepository.existsByFechaAndTourney(chargeResultsFechaDto.getFecha(), tourney))
-            throw new FechasIsAlreadyLoadedException(chargeResultsFechaDto.getFecha());
+       if(fechaTourneyRepository.existsByFechaAndTourney(chargeResultsFechaDto.getFecha(), tourney))
+         throw new FechasIsAlreadyLoadedException(chargeResultsFechaDto.getFecha());
 
         for(ResultDto resultDto : chargeResultsFechaDto.getResults()){
             results.add(Result.mapToResult(resultDto));
         }
 
-        fechaTourneyRepository.save(FechaTourney.mapToFechaTourney(chargeResultsFechaDto, tourney, results));
+        fechaTourneyRepository.save(FechaTourney.mapToFechaTourney(
+                chargeResultsFechaDto.getFecha(), tourney, results));
 
-        response.setTourney(chargeResultsFechaDto.getNameTourney());
-        response.setNameUser(FECHA + chargeResultsFechaDto.getFecha());
-        response.setFecha(chargeResultsFechaDto.getFecha());
-        response.setResults(results.stream()
+        return ChargeResultResponse.builder()
+                .tourney(chargeResultsFechaDto.getNameTourney())
+                .nameUser(FECHA + chargeResultsFechaDto.getFecha())
+                .fecha(chargeResultsFechaDto.getFecha())
+                .results(results.stream()
                         .map(ResultResponse::mapFromResult)
-                        .collect(Collectors.toList()));
-
-        return response;
+                        .collect(Collectors.toList()))
+                .build();
     }
+
+    @Override
+    public ChargeResultResponse chargeResultFecha(ChargeResultsFechaDto chargeResultsFechaDto) {
+
+        List<Result> results = new ArrayList<>();
+
+        FechaTourney fechaTourney = fechaTourneyRepository.getFechaTourneyByFechaAndTourney(
+                chargeResultsFechaDto.getFecha(),
+                chargeResultsFechaDto.getNameTourney(),
+                chargeResultsFechaDto.getYear()).orElseThrow(FechaIsNotChargeException::new);
+
+        results = modifiedResultsEmpties(chargeResultsFechaDto);
+
+        FechaTourney fechaTourneyWithResults =  fechaTourneyRepository.getFechaTourneysById(fechaTourney.getId());
+        fechaTourneyWithResults.setResults(results);
+        fechaTourneyRepository.save(fechaTourneyWithResults);
+
+        return ChargeResultResponse.builder()
+                .tourney(chargeResultsFechaDto.getNameTourney())
+                .nameUser(FECHA + chargeResultsFechaDto.getFecha())
+                .fecha(chargeResultsFechaDto.getFecha())
+                .results(results.stream()
+                        .map(ResultResponse::mapFromResult)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+
+    private List<Result> modifiedResultsEmpties(ChargeResultsFechaDto chargeResultsFechaDto){
+
+        List<Result> results = new ArrayList<>();
+
+        List<Result> resultsCharges = fechaTourneyRepository.getResultsByTourneyAndFecha(
+                chargeResultsFechaDto.getNameTourney(),
+                chargeResultsFechaDto.getYear(),
+                chargeResultsFechaDto.getFecha())
+                .orElseThrow(FechaIsNotChargeException::new);
+
+        for(Result result : resultsCharges){
+
+            if(Objects.isNull(result.getGolLocalTeam())){
+
+                for(ResultDto resultDto : chargeResultsFechaDto.getResults()){
+
+                    if(resultDto.getLocalTeam().equals(result.getLocalTeam())
+                            && resultDto.getVisitingTeam().equals(result.getVisitingTeam())){
+
+                        result.setGolLocalTeam(resultDto.getGolLocalTeam());
+                        result.setGolVisitingTeam(resultDto.getGolVisitingTeam());
+                        results.add(result);
+                        break;
+                    }
+                }
+            }
+            if (!results.contains(result)) {
+                results.add(result);
+            }
+        }
+        return results;
+    }
+
 }
